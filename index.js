d41ae07196ab32e0fbfaee8bba26a7259d9a9852
Hani Amer <cvba2222@gmail.com>
2022-12-24 01:06:19 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(arrOfObj) {
+    const result = arrOfObj.find(element => element.result === "W")
+    if (result) {
+        return result.year
+    }
+    else 
+        "Undefined"
+}
